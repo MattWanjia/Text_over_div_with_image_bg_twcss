84100a480ef684b158125edd4785a390af776829
MattWanjia <wanjiamartin@gmail.com>
2023-02-23 15:07:56 +0300

    first commit

---
+++ components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <div class='h-screen w-screen relaive'>
+        <img class='w-full h-full' src='nairobi1.jpeg'/>
+        <div class='absolute inset-0'>
+            <div class='w-full h-full'>
+                <p>Hello</p>
+            </div>
+        </div>
+    </div>
+  )
+}
